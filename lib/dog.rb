4667859a8d29aa21390686df00f1220e337a1b57
Kyle Bradley <kylepersonalmail@gmail.com>
2020-04-01 20:03:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,17 @@
 class Dog
-  def initialize
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  
 
 end
